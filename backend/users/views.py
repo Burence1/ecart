178147d2a99ded7aa9bca55368bcf0fc3502cb52
Burence1 <burensomondi@gmail.com>
2021-07-12 18:39:52 +0300

    add register serializer

--- backend/users/views.py
+++ backend/users/views.py
@@ -1,3 +1,21 @@
 from django.shortcuts import render
+from .serializers import *
+from rest_framework import serializers
+from rest_framework.response import Response
+from rest_framework.exceptions import AuthenticationFailed
+from rest_framework.views import APIView
+import jwt
+import datetime
+from .models import User
 
 # Create your views here.
+
+
+class RegisterView(APIView):
+    serializer_class = UserSerializer
+
+    def post(self, request, format=None):
+        serializer = self.serializer_class(data=request.data)
+        serializer.is_valid(raise_exception=True)
+        serializer.save()
+        return Response(serializer.data)
