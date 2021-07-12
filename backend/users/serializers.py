a550ea954a4599c20c40a932258177cf2f586cca
Burence1 <burensomondi@gmail.com>
2021-07-12 17:41:44 +0300

    add cart serializers

--- backend/users/serializers.py
+++ backend/users/serializers.py
@@ -6,16 +6,27 @@ from .models import *
 class UserSerializer(serializers.ModelSerializer):
   class Meta:
     model = User
-    fields = ['id','username','email','bio','avatar']
+    fields = ['id','username','email','bio','avatar','password','is_verified']
     extra_kwargs = {
         'password': {'write_only': True, }
     }
 
     def create(self, validated_data):
-      newUser = User(
+      regUser = User(
           email=validated_data['email'],
           username=validated_data['username']
       )
-      newUser.set_password(validated_data['password'])
-      newUser.save()
-      return newUser
+      regUser.set_password(validated_data['password'])
+      regUser.save()
+      return regUser
+
+class CartSerializer(serializers.ModelSerializer):
+  
+  product_name = serializers.CharField(source='product.name')
+  product_price = serializers.DecimalField(decimal_places=2, max_digits=6, source='product.price')
+  product_model = serializers.CharField(source='product.prod_model')
+  product_image = serializers.ImageField(source='product.image')
+
+  class Meta:
+      model = Cart
+      exclude = []
