04d03c1e453bc9b271567264270ffbf47260d503
Burence1 <burensomondi@gmail.com>
2021-07-12 19:47:38 +0300

    test endpoints

--- backend/ecomm/urls.py
+++ backend/ecomm/urls.py
@@ -14,8 +14,18 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+from django.urls import path,include
+from django.conf import settings
+from django.conf.urls.static import static
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('user/', include('users.urls')),
+    path('', include('products.urls')),
+    path('category/', include('products.urls')),
+    path('Brand/', include('products.urls')),
+    path('product/', include('products.urls')),
 ]
+
+urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
+urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
